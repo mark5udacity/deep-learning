df1bfb82fbd388cb2df316f3021e7d0db8a1cc65
Mark 5 <markvudacity@gmail.com>
2017-12-28 19:32:11 -0500

    First attempt at param tunin', including:  -- Ups number of iterations  -- Sets hidden nodes to 6 in order to match the number of factors (season, time, etc)  -- When in doubt, keep calm and lower the learning rate (this time to 10% of original value)

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -125,7 +125,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+iterations = 1000
+learning_rate = 0.01
+hidden_nodes = 6
 output_nodes = 1
