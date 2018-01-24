b65e6cba6d2440aeb76bc0b14549bef134dc29a9
Mark 5 <markvudacity@gmail.com>
2018-01-24 14:51:12 -0500

    Playing around with different sizes for pooling and strides.

--- keras-tuts/pool-dims.py
+++ keras-tuts/pool-dims.py
@@ -1,6 +1,30 @@
 from keras.models import Sequential
 from keras.layers import MaxPooling2D
 
-model = Sequential()
-model.add(MaxPooling2D(pool_size=2, strides=2, input_shape=(100, 100, 15)))
-model.summary()
+
+i = 2
+while (i <= 100):
+    j = 1
+
+    while (j <= 100):
+        pool_size = i
+        strides = j
+
+        print("\n\nsize: {} - strides: {}".format(pool_size, strides))
+        model = Sequential()
+        model.add(MaxPooling2D(pool_size=pool_size, strides=strides, input_shape=(100, 100, 15)))
+        model.summary()
+        print("\n\n")
+
+        if j == 64:
+            j = 100
+        else:
+            j *= 2
+
+
+    if i == 64:
+        i = 100
+    else:
+        i *= 2
+
+print("All done!")
