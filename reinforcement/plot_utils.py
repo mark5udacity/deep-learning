7dcfdbecdeace7c1d9c8d83a88e4d5724c3f13cb
Mark 5 <markvudacity@gmail.com>
2018-03-25 20:56:57 -0400

    [RL-lessons TD] Added TD notebook.

--- reinforcement/plot_utils.py
+++ reinforcement/plot_utils.py
@@ -1,8 +1,24 @@
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import axes3d, Axes3D
+import seaborn as sns
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from mpl_toolkits.mplot3d import Axes3D
 
+sns.set_style("white")
+
+def plot_values(V):
+	# reshape the state-value function
+	V = np.reshape(V, (4,12))
+	# plot the state-value function
+	fig = plt.figure(figsize=(15,5))
+	ax = fig.add_subplot(111)
+	im = ax.imshow(V, cmap='cool')
+	for (j,i),label in np.ndenumerate(V):
+	    ax.text(i, j, np.round(label,3), ha='center', va='center', fontsize=14)
+	plt.tick_params(bottom='off', left='off', labelbottom='off', labelleft='off')
+	plt.title('State-Value Function')
+	plt.show()
 
 def plot_blackjack_values(V):
     def get_Z(x, y, usable_ace):
